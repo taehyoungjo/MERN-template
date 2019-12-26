b2c17fc3a1309297da46f21e2150f93181100f3f
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2019-12-26 10:28:52 -0800

    ch5

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://thjo1028:thjo1028@cluster0-ltqqb.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://thjo1028:thjo1028@cluster0-ou56r.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
