fbac3573150cf2e08c733e3f0617f9ce23217fb0
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2019-12-30 15:37:02 -0800

    Posts

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://thjo1028:thjo1028@cluster0-ou56r.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
