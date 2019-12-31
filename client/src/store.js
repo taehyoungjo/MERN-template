5303de4b1721ebf782a09ffc92ec3e31d5073ed9
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2019-12-30 17:40:48 -0800

    redux devtools for production

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
+var composition;
+
+// Check if in production
+if (process.env.NODE_ENV === "production") {
+  composition = compose(applyMiddleware(...middleware));
+} else {
+  composition = compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  );
+}
+
+const store = createStore(rootReducer, initialState, composition);
 
 export default store;
